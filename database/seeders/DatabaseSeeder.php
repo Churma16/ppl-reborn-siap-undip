d44ec0e48e37e32010ae0e1cd900568e1ab95cd4
Fathan <74785057+Churma16@users.noreply.github.com>
2023-04-06 14:45:33 +0700

    adding seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,18 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\PKL;
+use App\Models\Provinsi;
+use App\Models\Skripsi;
+use App\Models\User;
+use App\Models\KHS;
+use App\Models\IRS;
+use App\Models\Kabupaten;
+use App\Models\Mahasiswa;
+use App\Models\Dosen;
+
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +32,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Provinsi::factory(10)->create();
+        Kabupaten::factory(20)->create();
+        Dosen::factory(20)->create();
+        Mahasiswa::factory(20)->create();
+        IRS::factory(20)->create();
+        KHS::factory(20)->create();
+        PKL::factory(15)->create();
+        Skripsi::factory(10)->create();
+        User::factory(39)->create();
+
     }
 }
