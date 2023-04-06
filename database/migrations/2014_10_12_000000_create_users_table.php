cc5943c1fd6ce214bb3b0a688ebddcc730b45852
Fathan <74785057+Churma16@users.noreply.github.com>
2023-04-06 13:29:13 +0700

    editing user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('username')->unique();
             $table->string('nama');
             $table->string('role');
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->timestamps();
         });
