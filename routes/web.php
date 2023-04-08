6945f97f17ee10ece638c0fb76e79e2ddad41331
Fathan <74785057+Churma16@users.noreply.github.com>
2023-04-08 20:33:34 +0700

    making layouts and partials

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardDepartemenController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard-master');
 });
+
+Route::get('/dashboard-departemen', [DashboardDepartemenController::class, 'index']);
+
