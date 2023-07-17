7d85cbc3cfcf97aa5bdef68c8484db4b4c7bb367
Fathan <74785057+Churma16@users.noreply.github.com>
2023-07-18 01:46:15 +0700

    add readability

--- resources/views/dashboard-mahasiswa/layouts/main.blade.php
+++ resources/views/dashboard-mahasiswa/layouts/main.blade.php
@@ -1,3 +1,4 @@
+<!-- MATERIAL DASHBOARD 2 -->
 <!--
 =========================================================
 * Material Dashboard 2 - v3.0.5
@@ -16,35 +17,41 @@
 <html lang="en">
 
 <head>
+    <!-- Meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/apple-icon.png" />
-    <link rel="icon" type="image/png" href="/assets/img/favicon.png" />
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="76x76" href="/img/logo-undip.png" />
+    <link rel="icon" type="image/png" href="/img/logo-undip.png" />
+
+    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Page title -->
     <title>Dashboard Mahasiswa</title>
 
+    <!-- Custom styles -->
     @yield('styles')
-    <!--     Fonts and icons     -->
+
+    <!-- Fonts and icons -->
     <link rel="stylesheet" type="text/css"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
-    <!-- Nucleo Icons -->
-    <link href="/assets/css/nucleo-icons.css" rel="stylesheet" />
-    <link href="/assets/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <!-- Material Icons -->
+    <link href="/assets/css/nucleo-icons.css" rel="stylesheet" /> <!-- Nucleo Icons -->
+    <link href="/assets/css/nucleo-svg.css" rel="stylesheet" /> <!-- Nucleo SVG Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet" />
-    <!-- CSS Files -->
+
+    <!-- Material Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script> <!-- Font Awesome Icons -->
+
+    <!-- Material Dashboard CSS -->
     <link id="pagestyle" href="/assets/css/material-dashboard.css?v=3.0.5" rel="stylesheet" />
-    <!-- Nepcha Analytics (nepcha.com) -->
-    <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
+
+    <!-- Nepcha Analytics -->
     <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
 
-
+    <!-- Feather Icons -->
     <script src="https://unpkg.com/feather-icons"></script>
-
-
 </head>
 
 <body class="g-sidenav-show bg-gray-200">
@@ -62,34 +69,36 @@
             @include('partials.footer')
         </div>
     </main>
+
     @include('partials.plugin')
 
-
-
-    <!--   Core JS Files   -->
+    <!-- Core JS Files -->
     <script src="/assets/js/core/popper.min.js"></script>
     <script src="/assets/js/core/bootstrap.min.js"></script>
     <script src="/assets/js/plugins/perfect-scrollbar.min.js"></script>
     <script src="/assets/js/plugins/smooth-scrollbar.min.js"></script>
     <script src="/assets/js/plugins/chartjs.min.js"></script>
+
+    <!-- Custom scripts -->
     @yield('scripts')
+
     <script>
         var win = navigator.platform.indexOf("Win") > -1;
         if (win && document.querySelector("#sidenav-scrollbar")) {
             var options = {
                 damping: "0.5",
             };
-            Scrollbar.init(
-                document.querySelector("#sidenav-scrollbar"),
-                options
-            );
+            Scrollbar.init(document.querySelector("#sidenav-scrollbar"), options);
         }
     </script>
+
     <!-- Github buttons -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
-    <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+
+    <!-- Material Dashboard JS -->
     <script src="/assets/js/material-dashboard.min.js?v=3.0.5"></script>
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+    <!-- Feather Icons -->
     <script>
         feather.replace()
     </script>
