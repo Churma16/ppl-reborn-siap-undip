c0083856a0d418b203a5ff8285a7122f76d5afbf
Fathan <74785057+Churma16@users.noreply.github.com>
2025-12-15 19:58:11 +0700

    add: add comment for each function

--- app/Models/PKL.php
+++ app/Models/PKL.php
@@ -15,6 +15,14 @@ class PKL extends Model
         return $this->belongsTo(Mahasiswa::class);
     }
 
+    /**
+     * This function counts the number of PKL records with the status "lulus" and "belum lulus" and returns
+     * the counts as an associative array.
+     * 
+     * @return an associative array with two keys: 'lulus' and 'belum_lulus'. The value of 'lulus' is the
+     * count of PKL records where the status_lulus is "lulus", and the value of 'belum_lulus' is the count
+     * of PKL records where the status_lulus is "belum lulus".
+     */
     public function getJumlahKelulusanAttribute()
     {
         // Count the number of PKL records where status_lulus is "lulus"
@@ -27,18 +35,34 @@ class PKL extends Model
         return ['lulus' => $lulus_count, 'belum_lulus' => $belum_lulus_count];
     }
 
+    /**
+     * The function "getTanggalMulaiFormattedAttribute" formats a given date in the "d M Y" format.
+     * 
+     * @return The formatted date in the format "d M Y" is being returned.
+     */
     public function getTanggalMulaiFormattedAttribute()
     {
         $date = Carbon::parse($this->tanggal_mulai);
         return $date->format('d M Y');
     }
 
+    /**
+     * The function "getTanggalSelesaiFormattedAttribute" formats a given date in the "d M Y" format.
+     * 
+     * @return The formatted date in the format "d M Y" is being returned.
+     */
     public function getTanggalSelesaiFormattedAttribute()
     {
         $date = Carbon::parse($this->tanggal_selesai);
         return $date->format('d M Y');
     }
 
+    /**
+     * The function "getTanggalDiunggahAttribute" returns the formatted date of the "created_at" attribute
+     * in the format "dd M Y".
+     * 
+     * @return the formatted date of the "created_at" attribute in the format "d M Y" (e.g. 01 Jan 2022).
+     */
     public function getTanggalDiunggahAtrribute()
     {
         $date = Carbon::parse($this->created_at);
--- app/Models/Skripsi.php
+++ app/Models/Skripsi.php
@@ -12,6 +12,14 @@ class Skripsi extends Model
     protected $guarded = ['id'];
 
 
+    /**
+     * The function `getJumlahKelulusanAttribute` counts the number of PKL records with the status "lulus"
+     * and "belum lulus" and returns the counts as an associative array.
+     * 
+     * @return an associative array with two keys: 'lulus' and 'belum_lulus'. The value of 'lulus' is the
+     * count of PKL records where the 'status_skripsi' is 'lulus', and the value of 'belum_lulus' is the
+     * count of PKL records where the 'status_skripsi' is 'belum lulus'.
+     */
     public function getJumlahKelulusanAttribute()
     {
         // Count the number of PKL records where status_lulus is "lulus"
@@ -23,12 +31,23 @@ class Skripsi extends Model
         // Return the counts as an associative array
         return ['lulus' => $lulus_count, 'belum_lulus' => $belum_lulus_count];
     }
+    
+    /**
+     * The function "getTanggalMulaiFormattedAttribute" formats a given date in the "d M Y" format.
+     * 
+     * @return The formatted date in the format "d M Y" is being returned.
+     */
     public function getTanggalMulaiFormattedAttribute()
     {
         $date = Carbon::parse($this->tanggal_mulai);
         return $date->format('d M Y');
     }
 
+    /**
+     * The function "getTanggalSelesaiFormattedAttribute" formats a given date in the "d M Y" format.
+     * 
+     * @return The formatted date in the format "d M Y" is being returned.
+     */
     public function getTanggalSelesaiFormattedAttribute()
     {
         $date = Carbon::parse($this->tanggal_sidang);
