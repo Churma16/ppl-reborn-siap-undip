6e922a362b9aa7650f7623003987340bd9f51223
Fathan <74785057+Churma16@users.noreply.github.com>
2025-12-15 19:58:45 +0700

    add: make new migration for semester table

---
+++ database/migrations/2025_12_10_015032_create_semesters_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('semesters', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_semester');
+            $table->string('kode_semester')->unique();
+            $table->boolean('is_active')->default(true);
+            $table->date('tanggal_mulai');
+            $table->date('tanggal_selesai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('semesters');
+    }
+};
