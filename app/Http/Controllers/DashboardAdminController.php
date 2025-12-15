e44ae2080eb4644ecca2e735a87be354fb4b9d21
Fathan <74785057+Churma16@users.noreply.github.com>
2025-12-15 20:05:04 +0700

    Add PHPDoc comments and improve data retrieval
    Added detailed PHPDoc comments to methods in DashboardAdminController for better documentation. Improved data retrieval in dataMahasiswa() by fetching dosen data and passing it to the view. Also added a stub for createSemester method.

--- app/Http/Controllers/DashboardAdminController.php
+++ app/Http/Controllers/DashboardAdminController.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Provinsi;
-use App\Models\Kabupaten;
-use App\Models\Dosen;
-use App\Models\Mahasiswa;
 use App\Models\User;
+use App\Models\Dosen;
+use App\Models\Provinsi;
+use App\Models\Semester;
+use App\Models\Kabupaten;
+use Carbon\Carbon;
 
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
 
 class DashboardAdminController extends Controller
 {
+    /**
+     * The index function returns a view with the title "Dashboard Admin".
+     *
+     * @return a view called 'dashboard-admin.index' with a title of 'Dashboard Admin'.
+     */
     public function index()
     {
 
@@ -20,6 +27,12 @@ class DashboardAdminController extends Controller
         ]);
     }
 
+    /**
+     * The function `tambahMahasiswaBaru()` returns a view with data for adding a new student, including a
+     * title, a list of provinces, a list of districts, and a list of professors.
+     *
+     * @return a view called 'dashboard-admin.tambah-mahasiswa-baru' with the following data:
+     */
     public function tambahMahasiswaBaru()
     {
         $provinsis = Provinsi::all();
@@ -34,6 +47,15 @@ class DashboardAdminController extends Controller
         ]);
     }
 
+    /**
+     * The function creates a new Mahasiswa (student) record and associated user account in a database.
+    *
+    * @param Request request The  parameter is an instance of the Request class, which represents
+     * an HTTP request. It contains all the data and information about the incoming request, such as the
+     * request method, headers, and request payload.
+     *
+     * @return a redirect response to the '/dashboard-admin' route with a success flash message.
+     */
     public function createMahasiswa(Request $request)
     {
 
@@ -59,14 +81,29 @@ class DashboardAdminController extends Controller
         return redirect('/dashboard-admin')->with('success', 'New post has been added!');
     }
 
+    /**
+     * The function "dataMahasiswa" retrieves data from the "Mahasiswa" and "Dosen" models and passes them
+     * to the "data-mahasiswa" view.
+     *
+     * @return a view called 'dashboard-admin.data-mahasiswa' with three variables: 'mahasiswas',
+     * 'angkatans', and 'dosens'.
+    */
     public function dataMahasiswa()
     {
         $mahasiswa = Mahasiswa::all();
         $angkatan = Mahasiswa::select('angkatan')->distinct()->get();
-
+        $dosens = Dosen::get('kode_wali', 'nama');
         return view('dashboard-admin.data-mahasiswa', [
             'mahasiswas' => $mahasiswa,
             'angkatans' => $angkatan,
+            'dosens' => $dosens,
         ]);
     }
+
+
+    public function createSemester(Request $request)
+    {
+
+    }
 }
+
