e0401976b9fe30a23ff6f10bd0fed0801b065081
Fathan <74785057+Churma16@users.noreply.github.com>
2025-12-20 14:54:36 +0700

    Add login route and user resource routes with authentication middleware

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Route::get('/profile', function (Request $request) {
+    //     return $request->user();
+    // });
+    Route::apiResource('/user', UserController::class);
 });
