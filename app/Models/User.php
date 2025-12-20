b66e899c180592e0f7c2386e54f03c780105cf49
Fathan <74785057+Churma16@users.noreply.github.com>
2025-12-20 14:50:10 +0700

    Set 'nip_nim' as route key and cast 'role' attribute
    Updated the User model to use 'nip_nim' as the route key by overriding getRouteKeyName. Also added casting for the 'role' attribute to the userRole enum and improved import organization.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,17 +3,23 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Dosen;
+use App\Enums\userRole;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-use App\Models\Dosen;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
     protected $primaryKey = 'nip_nim';
+
+    public function getRouteKeyName()
+    {
+        return 'nip_nim';
+    }
     /**
      * The attributes that are mass assignable.
      *
@@ -28,9 +34,9 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+    *
+    * @var array<int, string>
+    */
     protected $hidden = [
         'password',
         'remember_token',
@@ -38,11 +44,12 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+    *
+    * @var array<string, string>
+    */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'role' => userRole::class,
     ];
 
     public function dosen()
