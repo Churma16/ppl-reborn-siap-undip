1f6f86cc0b1f9ffc9bd63f6c78edb600c7c7c702
Fathan <74785057+Churma16@users.noreply.github.com>
2025-12-24 23:53:03 +0700

    Update main layout for Siap Undip: modify meta tags and canonical link

--- resources/views/login/layouts/main.blade.php
+++ resources/views/login/layouts/main.blade.php
@@ -25,31 +25,24 @@
     </title>
 
     @yield('styles')
-    <link rel="canonical" href="https://www.creative-tim.com/product/material-dashboard" />
+    <link rel="canonical" href="{{ url('/') }}" />
 
     <meta name="keywords"
-        content="creative tim, html dashboard, html css dashboard, web dashboard, bootstrap 5 dashboard, bootstrap 5, css3 dashboard, bootstrap 5 admin, Material Dashboard bootstrap 5 dashboard, frontend, responsive bootstrap 5 dashboard, free dashboard, free admin dashboard, free bootstrap 5 admin dashboard">
+        content="siap undip, student information system, undip dashboard, academic portal">
     <meta name="description"
-        content="Material Dashboard 2 is a beautiful Bootstrap 5 admin dashboard with a large number of components, designed to look beautiful and organized. If you are looking for a tool to manage and visualize data about your business, this dashboard is the thing for you.">
+        content="Siap Undip adalah sistem informasi akademik terintegrasi untuk mahasiswa Universitas Diponegoro.">
 
-    <meta name="twitter:card" content="product">
-    <meta name="twitter:site" content="@creativetim">
-    <meta name="twitter:title" content="Material Dashboard 2 by Creative Tim">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Siap Undip - Login">
     <meta name="twitter:description"
-        content="Material Dashboard 2 is a beautiful Bootstrap 5 admin dashboard with a large number of components, designed to look beautiful and organized. If you are looking for a tool to manage and visualize data about your business, this dashboard is the thing for you.">
-    <meta name="twitter:creator" content="@creativetim">
-    <meta name="twitter:image"
-        content="https://s3.amazonaws.com/creativetim_bucket/products/450/original/material-dashboard.jpg">
+        content="Siap Undip adalah sistem informasi akademik terintegrasi untuk mahasiswa Universitas Diponegoro.">
 
-    <meta property="fb:app_id" content="655968634437471">
-    <meta property="og:title" content="Material Dashboard 2 by Creative Tim" />
-    <meta property="og:type" content="article" />
-    <meta property="og:url" content="http://demos.creative-tim.com/material-dashboard/pages/dashboard.html" />
-    <meta property="og:image"
-        content="https://s3.amazonaws.com/creativetim_bucket/products/450/original/material-dashboard.jpg" />
+    <meta property="og:title" content="Siap Undip - Login" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="{{ url('/') }}" />
     <meta property="og:description"
-        content="Material Dashboard 2 is a beautiful Bootstrap 5 admin dashboard with a large number of components, designed to look beautiful and organized. If you are looking for a tool to manage and visualize data about your business, this dashboard is the thing for you." />
-    <meta property="og:site_name" content="Creative Tim" />
+        content="Siap Undip adalah sistem informasi akademik terintegrasi untuk mahasiswa Universitas Diponegoro." />
+    <meta property="og:site_name" content="Siap Undip" />
 
     <link rel="stylesheet" type="text/css"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
@@ -61,88 +54,24 @@
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
 
-    <link id="pagestyle" href="/assets/css/material-dashboard.min.css?v=3.1.0" rel="stylesheet" />
+    <link id="pagestyle" href="/assets/css/material-dashboard.css?v=3.1.0" rel="stylesheet" />
 
     <style>
         .async-hide {
             opacity: 0 !important
         }
     </style>
-    <script>
-        (function(a, s, y, n, c, h, i, d, e) {
-            s.className += ' ' + y;
-            h.start = 1 * new Date;
-            h.end = i = function() {
-                s.className = s.className.replace(RegExp(' ?' + y), '')
-            };
-            (a[n] = a[n] || []).hide = h;
-            setTimeout(function() {
-                i();
-                h.end = null
-            }, c);
-            h.timeout = c;
-        })(window, document.documentElement, 'async-hide', 'dataLayer', 4000, {
-            'GTM-K9BGS8K': true
-        });
-    </script>
-
-    <script>
-        (function(i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r] = i[r] || function() {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-            a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-            a.async = 1;
-            a.src = g;
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-        ga('create', 'UA-46172202-22', 'auto', {
-            allowLinker: true
-        });
-        ga('set', 'anonymizeIp', true);
-        ga('require', 'GTM-K9BGS8K');
-        ga('require', 'displayfeatures');
-        ga('require', 'linker');
-        ga('linker:autoLink', ["2checkout.com", "avangate.com"]);
-    </script>
-
-
-    <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-NKDMSK6');
-    </script>
-
-
 
     <script defer data-site="demos.creative-tim.com" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
 </head>
 
 <body class="bg-gray-200">
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKDMSK6" height="0" width="0"
-            style="display:none;visibility:hidden"></iframe></noscript>
-
     @include('login.partials.navbar')
 
     <main class="main-content  mt-0">
         <div class="page-header align-items-start min-vh-100"
-            style="background-image: url('/img/backgroundUndip.jpg'); -webkit-backdrop-filter: blur(2px);
-            backdrop-filter: blur(2px);">
+            style="background-image: url('/img/backgroundUndip.jpg'); background-color: rgba(16, 30, 70, 0.8);">
             @yield('content')
-
 
             @include('login.partials.footer')
         </div>
@@ -163,13 +92,7 @@
         }
     </script>
 
-    {{-- <script async defer src="https://buttons.github.io/buttons.js"></script> --}}
-
     <script src="/assets/js/material-dashboard.min.js?v=3.1.0"></script>
-    {{-- <script defer src="https://static.cloudflareinsights.com/beacon.min.js/v52afc6f149f6479b8c77fa569edb01181681764108816"
-        integrity="sha512-jGCTpDpBAYDGNYR5ztKt4BQPGef1P0giN6ZGVUi835kFF88FOmmn8jBQWNgrNd8g/Yu421NdgWhwQoaOPFflDw=="
-        data-cf-beacon='{"rayId":"7e07e81608f39e35","version":"2023.4.0","r":1,"token":"1b7cbb72744b40c580f8633c6b62637e","si":100}'
-        crossorigin="anonymous"></script> --}}
 </body>
 
 </html>
