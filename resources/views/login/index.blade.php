d3258f463cb861df40edb7c6f683bf58238e0f35
Fathan <74785057+Churma16@users.noreply.github.com>
2025-12-25 12:02:07 +0700

    Update card header background color for login page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -7,7 +7,7 @@
             <div class="col-lg-4 col-md-8 col-12 mx-auto">
                 <div class="card z-index-0 fadeIn3 fadeInBottom">
                     <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                        <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                        <div class="bg-gradient-dark shadow-primary border-radius-lg py-3 pe-1">
                             <h4 class="text-white font-weight-bolder text-center my-1 ">Sistem Informasi Akademik<br>Universitas Diponegoro</h4>
                         </div>
                     </div>
