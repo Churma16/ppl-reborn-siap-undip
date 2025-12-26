48c93d99fd426d7bf60bbc60aa888da2a5daab5c
Fathan <74785057+Churma16@users.noreply.github.com>
2025-12-26 16:44:48 +0700

    Enhance DashboardDepartemenController and index view with additional student statistics, including average GPA from the previous semester, graduation candidates count, and critical students list. Update visual elements to reflect new data.

--- app/Http/Controllers/DashboardDepartemenController.php
+++ app/Http/Controllers/DashboardDepartemenController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Mahasiswa;
 use App\Models\Dosen;
+use App\Models\KHS;
 use App\Models\Pkl;
+use App\Models\Semester;
 use App\Models\Skripsi;
 
 use Illuminate\Http\Request;
@@ -23,9 +25,35 @@ class DashboardDepartemenController extends Controller
         $pkl = new Pkl();
         $skripsi = new Skripsi();
 
-        // $mahasiswas = Mahasiswa::all();
+        $semesterAktif = Semester::semesterAktif()->first();
+        $rerataMhsSemesterLalu = round(KHS::where('semester_id',  $semesterAktif->id)
+            ->avg('ip_semester'), 2);
 
+        $calonWisudaCount = Skripsi::where('status_skripsi', 'Lulus')
+            ->where('semester_id', $semesterAktif->id)
+            ->count();
+        // dd($calonWisudaCount);
 
+        $mhsKritis = Mahasiswa::whereRelation('irs', 'status_mahasiswa', 'Aktif')
+            ->whereHas('khsTerakhir', function ($q) {
+                $q->where('semester', '>', 2)
+                    ->where('ip_kumulatif', '<', 2.00);
+            })
+            ->count();
+
+        $listMhsKritis = Mahasiswa::whereRelation('irs', 'status_mahasiswa', 'Aktif')
+            ->whereHas(
+                'khsTerakhir',
+                fn($q) =>
+                $q->where('semester', '>', 2)
+                    ->where('ip_kumulatif', '<', 2.00)
+            )
+            ->with('khsTerakhir')
+            ->limit(15)
+            ->get();
+
+        $listMhsKritis = $listMhsKritis->sortBy('khsTerakhir.ip_kumulatif');
+        // dd($listMhsKritis);
         // $statusAktif = [];
         // foreach ($mahasiswas as $m) {
         //     $statusAktif[] = $m->getStatusAktifAttribute();
@@ -37,7 +65,7 @@ class DashboardDepartemenController extends Controller
         //         $jumlahMhsAktif++;
         //     }
         // }
-        
+
         // Retrieve all Mahasiswa records
         $mahasiswas = Mahasiswa::all();
 
@@ -48,14 +76,92 @@ class DashboardDepartemenController extends Controller
 
         // Count the number of Mahasiswa with status 'Aktif'
         $jumlahMhsAktif = $filteredMahasiswas->count();
+        // Combine PKL queries
+        $pklLulusSmtAktif = PKL::where('semester_id', $semesterAktif->id)
+            ->where('status_lulus', 'Lulus')->count();
 
+        $pklProgress = PKL::where('status_lulus', 'Belum Lulus')->distinct()->count();
+        $pklBelumAmbil = Mahasiswa::whereHas(
+            'khsTerakhir',
+            fn($q) =>
+            $q->where('sks_kumulatif', '>=', 105)
+        )
+            ->whereDoesntHave('pkl')
+            ->count();
+        // Combine Skripsi queries
+        $skripsiLulusSmtAktif = Skripsi::where('semester_id', $semesterAktif->id)
+            ->where('status_skripsi', 'Lulus')
+            ->count();
 
+        $skripsiProgress = Skripsi::where('status_skripsi', 'Belum Lulus')->distinct()->count();
+
+        $skripsiBelumAmbil = Mahasiswa::whereHas(
+            'khsTerakhir',
+            fn($q) =>
+            $q->where('sks_kumulatif', '>=', 144)
+        )
+            ->whereDoesntHave('skripsi')
+            ->count();
+
+        // dd($skripsiBelumAmbil);
+        $statusPklCount = [
+            'lulus' => $pklLulusSmtAktif,
+            'progress' => $pklProgress,
+            'belum_ambil' => $pklBelumAmbil,
+        ];
+
+        $statusSkripsiCount = [
+            'lulus' => $skripsiLulusSmtAktif,
+            'progress' => $skripsiProgress,
+            'belum_ambil' => $skripsiBelumAmbil,
+        ];
+
+        $bebanDoswal = Mahasiswa::with('dosen:kode_wali,nama')
+            ->whereRelation('irs', function ($q) use ($semesterAktif) {
+                $q->where('semester_id', $semesterAktif->id)
+                    ->where('status_mahasiswa', 'Aktif');
+            })
+            ->get()
+            ->pluck('dosen.nama')
+            ->countBy();
+
+        $bebanDoswal = $bebanDoswal->sortDesc();
+
+        $totalMahasiswaBlmLulus = Mahasiswa::whereDoesntHave(
+            'skripsi',
+            fn($q) =>
+            $q->where('status_skripsi', 'Lulus')
+        )
+            ->count();
+
+        $totalDosen = Dosen::count();
+
+        if ($totalDosen > 0) {
+            $rasio = round($jumlahMhsAktif / $totalDosen,);
+            $rasioDosenMahasiswa = "1 : " . $rasio;
+        } else {
+            $rasioDosenMahasiswa = "N/A";
+        }
+
+        $rasioColor = $rasio > 30 ? 'text-danger' : 'text-success';
+        // dd($bebanDoswal->keys());
         return view('dashboard-departemen.index', [
             'mahasiswa' => $mahasiswa,
             'dosen' => $dosen,
             'pkl' => $pkl,
             'skripsi' => $skripsi,
             'jumlahMhsAktif' => $jumlahMhsAktif,
+            'rerataMhsSemesterLalu' => $rerataMhsSemesterLalu,
+            'calonWisudaCount' => $calonWisudaCount,
+            'mhsKritis' => $mhsKritis,
+            'listMhsKritis' => $listMhsKritis,
+            'statusPklCount' => $statusPklCount,
+            'statusSkripsiCount' => $statusSkripsiCount,
+            'bebanDoswal' => $bebanDoswal,
+            // 'totalMahasiswaBlmLulus' => $totalMahasiswaBlmLulus,
+            // 'totalDosen' => $totalDosen,
+            'rasioDosenMahasiswa' => $rasioDosenMahasiswa,
+            'rasioColor' => $rasioColor,
         ]);
     }
 
--- resources/views/dashboard-departemen/index.blade.php
+++ resources/views/dashboard-departemen/index.blade.php
@@ -13,16 +13,17 @@
                     </div>
                     <div class="text-end pt-1">
                         <p class="text-sm mb-0 text-capitalize">
-                            Total Mahasiswa
+                            Rerata Ipk Semester Lalu
                         </p>
-                        <h4 class="mb-0">{{ $mahasiswa->jumlah_mahasiswa }}</h4>
+                        <h4 class="mb-0">{{ $rerataMhsSemesterLalu }}</h4>
                     </div>
                 </div>
+                <hr class="dark horizontal my-0" />
 
-                <div class="card-footer p-1">
-                    {{-- <p class="mb-0">
+                <div class="card-footer px-4 py-2">
+                    <p class="mb-0">
                         <span class="text-success text-sm font-weight-bolder">+55% </span>than last week
-                    </p> --}}
+                    </p>
                 </div>
             </div>
         </div>
@@ -31,20 +32,22 @@
                 <div class="card-header p-3 pt-2">
                     <div
                         class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute">
-                        <i class="material-icons opacity-10">people</i>
+                        <i class="material-icons opacity-10">school</i>
                     </div>
                     <div class="text-end pt-1">
                         <p class="text-sm mb-0 text-capitalize">
-                            Total Dosen
+                            Calon Wisuda
                         </p>
-                        <h4 class="mb-0">{{ $dosen->jumlah_dosen }}</h4>
+                        <h4 class="mb-0">{{ $calonWisudaCount }}</h4>
                     </div>
                 </div>
-
-                <div class="card-footer p-1">
-                    {{-- <p class="mb-0">
-                        <span class="text-success text-sm font-weight-bolder">+3% </span>than last month
-                    </p> --}}
+                <hr class="dark horizontal my-0" />
+                <div class="card-footer px-4 py-2">
+                    <p class="mb-0">
+                        <span
+                            class="text-{{ $calonWisudaDiff < 0 ? 'danger' : 'success' }} text-sm font-weight-bolder">{{ $calonWisudaDiff }}
+                        </span>dibanding semester lalu
+                    </p>
                 </div>
             </div>
         </div>
@@ -52,22 +55,20 @@
             <div class="card">
                 <div class="card-header p-3 pt-2">
                     <div
-                        class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
-                        <i class="material-icons opacity-10">person</i>
+                        class="icon icon-lg icon-shape bg-gradient-danger shadow-danger text-center border-radius-xl mt-n4 position-absolute">
+                        <i class="material-icons opacity-10">warning</i>
                     </div>
                     <div class="text-end pt-1">
                         <p class="text-sm mb-0 text-capitalize">
-                            Mahasiswa Aktif
+                            Mahasiswa Kritis
                         </p>
-                        <h4 class="mb-0">{{ $jumlahMhsAktif }}</h4>
+                        <h4 class="mb-0">{{ $mhsKritis }}</h4>
                     </div>
                 </div>
-
-                <div class="card-footer p-1">
-                    {{-- <p class="mb-0">
-                        <span class="text-danger text-sm font-weight-bolder">-2%</span>
-                        than yesterday
-                    </p> --}}
+                <hr class="dark horizontal my-0" />
+                <div class="card-footer px-4 py-2">
+                    <p class="mb-0">
+                        semester > 2 dengan ipk < 2.00 </p>
                 </div>
             </div>
         </div>
@@ -76,20 +77,107 @@
                 <div class="card-header p-3 pt-2">
                     <div
                         class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
-                        <i class="material-icons opacity-10">person_off</i>
+                        <i class="material-icons opacity-10">percent</i>
                     </div>
                     <div class="text-end pt-1">
                         <p class="text-sm mb-0 text-capitalize">
-                            Mahasiswa Non-Aktif
+                            Rasio Dosen/Mahasiswa
                         </p>
-                        <h4 class="mb-0">{{ $mahasiswa->jumlah_mahasiswa - $jumlahMhsAktif }}</h4>
+                        <h4 class="mb-0 {{ $rasioColor }}">{{ $rasioDosenMahasiswa }}</h4>
                     </div>
                 </div>
+                <hr class="dark horizontal my-0" />
+                <div class="card-footer px-4 py-2">
+                    <p class="mb-0 ">
+                        Beban Terberat: {{ $bebanDoswal->keys()->first() }} </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-8">
+            <div class="card">
+                <div class="card-body">
+                    <h5>Monitoring Tingkat Akhir</h5>
+                    <div class="chart">
+                        <canvas id="chart-pkl-skripsi" class="chart-canvas" height="200"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5>Beban Dosen</h5>
+                    <div class="chart">
+                        <canvas id="chart-beban-dosen" class="chart-canvas" height="200"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-                <div class="card-footer p-1">
-                    {{-- <p class="mb-0">
-                        <span class="text-success text-sm font-weight-bolder">+5% </span>than yesterday
-                    </p> --}}
+    <div class="row mt-4">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h5>Daftar Mahasiswa Perlu Perhatian</h5>
+                    <div class="table-responsive">
+                        <table class="table align-items-center mb-0">
+                            <thead>
+                                <tr>
+                                    <th class="text-uppercase text-secondary text-sm font-weight-bolder ">Nama
+                                    </th>
+
+                                    <th class="text-center text-uppercase text-secondary text-sm font-weight-bolder ">
+                                        Angkatan
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-sm font-weight-bolder  ps-2">
+                                        Semester</th>
+                                    <th class="text-center text-uppercase text-secondary text-sm font-weight-bolder ">
+                                        IPK</th>
+                                    <th class="text-center text-uppercase text-secondary text-sm font-weight-bolder ">
+                                        SKS</th>
+                                    <th class="text-center ">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    @foreach ($listMhsKritis as $mhsKritis)
+                                        <td class="">
+                                            <h6 class="text-secondary text-xs ps-2">{{ $mhsKritis->nama }}</h6>
+                                        </td>
+                                        <td class="align-middle text-center">
+
+                                            <span
+                                                class="text-secondary text-xs font-weight-normal">{{ $mhsKritis->angkatan }}</span>
+                                        </td>
+                                        <td class="align-middle text-center">
+
+                                            <span
+                                                class="text-secondary text-xs font-weight-normal">{{ $mhsKritis->khsTerakhir->semester }}</span>
+                                        </td>
+                                        <td class="align-middle text-center text-sm">
+                                            <span
+                                                class="text-secondary text-xs font-weight-normal">{{ $mhsKritis->khsTerakhir->ip_kumulatif }}</span>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span
+                                                class="text-secondary text-xs font-weight-normal">{{ $mhsKritis->khsTerakhir->sks_kumulatif }}</span>
+                                        </td>
+                                        <td class="align-middle text-center ">
+                                            <a class="btn btn-outline-info px-2 py-1" href="mailto:{{ $mhsKritis->email }}"
+                                                title="Kirim Email">
+                                                <i class="material-icons opacity-10">mail</i>
+                                            </a>
+                                        </td>
+                                </tr>
+                                @endforeach
+
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
@@ -1222,6 +1310,147 @@
                         ticks: {
                             display: false,
                         }
+                    },
+                },
+            },
+        });
+
+
+        var ctx3 = document.getElementById("chart-pkl-skripsi").getContext("2d");
+
+        new Chart(ctx3, {
+            type: "bar",
+            data: {
+                // Label Sumbu Y (Kategori)
+                labels: ["PKL", "Skripsi"],
+                datasets: [{
+                        label: "Sudah Lulus",
+                        data: [
+                            {{ $statusPklCount['lulus'] }}, // Data PKL Lulus
+                            {{ $statusSkripsiCount['lulus'] }} // Data Skripsi Lulus
+                        ],
+                        backgroundColor: "#66BB6A", // Hijau
+                        barThickness: 40, // Batang lebih tebal biar enak dilihat
+                    },
+                    {
+                        label: "Sedang Proses",
+                        data: [
+                            {{ $statusPklCount['progress'] }}, // Data PKL Proses
+                            {{ $statusSkripsiCount['progress'] }} // Data Skripsi Proses
+                        ],
+                        backgroundColor: "#FFA726", // Kuning
+                        barThickness: 40,
+                    },
+                    {
+                        label: "Belum Ambil",
+                        data: [
+                            {{ $statusPklCount['belum_ambil'] }}, // Data PKL Proses
+                            {{ $statusSkripsiCount['belum_ambil'] }} // Data Skripsi Proses
+                        ],
+                        backgroundColor: "#e94440", // Kuning
+                        barThickness: 40,
+                    },
+                ],
+            },
+            options: {
+                indexAxis: 'y',
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    }
+                },
+                scales: {
+                    x: {
+                        stacked: true,
+                        grid: {
+                            drawBorder: false,
+                            display: true,
+                            color: "#f0f2f5",
+                        },
+                        ticks: {
+                            display: true,
+                            color: "#b2b9bf",
+                        }
+                    },
+                    y: {
+                        stacked: true,
+                        grid: {
+                            drawBorder: false,
+                            display: false,
+                        },
+                        ticks: {
+                            color: "#344767",
+                            font: {
+                                weight: 600, // Tebalkan font label "PKL" & "Skripsi"
+                                size: 14
+                            }
+                        }
+                    },
+                },
+            },
+        });
+
+        var ctx5 = document.getElementById("chart-beban-dosen").getContext("2d");
+
+        new Chart(ctx5, {
+            type: "bar", // Tipe Bar Biasa (Vertikal)
+            data: {
+                labels: @json($bebanDoswal->keys()), // Dummy data
+                datasets: [{
+                    label: "Jumlah Mahasiswa",
+                    tension: 0.4,
+                    borderWidth: 0,
+                    borderRadius: 4,
+                    borderSkipped: false,
+                    backgroundColor: "#344767", // Warna Gelap (Dark Blue) biar elegan
+                    data: @json($bebanDoswal->values()), // Dummy data
+                    maxBarThickness: 60 // Jangan terlalu gemuk barnya
+                }],
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        display: false, // Tidak butuh legend karena cuma 1 kategori
+                    }
+                },
+                scales: {
+                    y: {
+                        grid: {
+                            drawBorder: true,
+                            display: true,
+                            drawOnChartArea: true,
+                            drawTicks: false,
+                            borderDash: [5, 5],
+                            color: '#c1c4ce5c'
+                        },
+                        ticks: {
+                            display: true,
+                            color: "#b2b9bf",
+                        }
+                    },
+                    x: {
+                        grid: {
+                            drawBorder: false,
+                            display: false,
+                            drawOnChartArea: false,
+                            drawTicks: false,
+                            borderDash: [5, 5]
+                        },
+                        ticks: {
+                            display: true,
+                            color: "#344767", // Warna Nama Dosen
+                            padding: 10,
+                            font: {
+                                size: 11, // Agak kecil biar nama panjang muat
+                                family: "Roboto",
+                                style: 'normal',
+                                lineHeight: 2
+                            },
+                        },
                     },
                 },
             },
