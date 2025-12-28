f4c0e9c2c306f5e9a82fa0021f786f772c71e66b
Fathan <74785057+Churma16@users.noreply.github.com>
2025-12-28 19:41:42 +0700

    Refactor route definitions to use UserRole enum for role-based middleware and improve organization of controller imports

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,22 @@
 <?php
 
 use App\Enums\UserRole;
-use App\Http\Controllers\AdminKelolaSemester;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\BerandaGuestController;
-use App\Http\Controllers\DashboardAdminController;
-use App\Http\Controllers\DashboardDepartemenController;
-use App\Http\Controllers\DashboardDosenController;
-use App\Http\Controllers\DashboardMahasiswaController;
-use App\Http\Controllers\DashboardMhsController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\MahasiswaKelolaIrs;
 use App\Http\Controllers\MahasiswaKelolaKhs;
 use App\Http\Controllers\MahasiswaKelolaPkl;
+use App\Http\Controllers\BerandaGuestController;
+use App\Http\Controllers\DashboardMhsController;
 use App\Http\Controllers\MahasiswaKelolaSkripsi;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\SemesterController;
+use App\Http\Controllers\DashboardAdminController;
+use App\Http\Controllers\DashboardDosenController;
+use App\Http\Controllers\Admin\MahasiswaController;
+use App\Http\Controllers\Admin\MataKuliahController;
+use App\Http\Controllers\DashboardMahasiswaController;
+use App\Http\Controllers\DashboardDepartemenController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,7 +38,7 @@ Route::get('/logout', [LoginController::class, 'logout']);
 Route::resource('/', BerandaGuestController::class);
 
 // Departemen
-Route::middleware(['auth', 'role:2'])->group(function () {
+Route::middleware(['auth', 'role:' . UserRole::Departemen->value])->group(function () {
     Route::get('/dashboard-departemen', [DashboardDepartemenController::class, 'index']);
 
     Route::get('/dashboard-departemen/data-mahasiswa', [DashboardDepartemenController::class, 'dataMahasiswa']);
@@ -45,7 +47,7 @@ Route::middleware(['auth', 'role:2'])->group(function () {
 });
 
 // Dosen
-Route::middleware(['auth', 'role:3'])->group(function () {
+Route::middleware(['auth', 'role:' . UserRole::Dosen->value])->group(function () {
     Route::get('/dashboard-dosen', [DashboardDosenController::class, 'index']);
 
     Route::get('/dashboard-dosen/verifikasi-irs', [DashboardDosenController::class, 'verifikasiIrs']);
@@ -63,10 +65,13 @@ Route::middleware(['auth', 'role:3'])->group(function () {
     Route::get('/dashboard-dosen/verifikasi-skripsi', [DashboardDosenController::class, 'verifikasiSkripsi']);
     Route::get('/dashboard-dosen/verifikasi-skripsi/{action}/{skripsi}', [DashboardDosenController::class, 'verifikasiSkripsiKeputusan'])
         ->where('action', 'terima|tolak');
+
+    Route::get('/dashboard-dosen/validasi/table', [DashboardDosenController::class, 'getValidationTable']);
+    Route::post('/dashboard-dosen/validasi/{id}/{action}/{type}', [DashboardDosenController::class, 'verifyDashboardRequest']);
 });
 
 // Mahasiswa
-Route::middleware(['auth', 'role:'.UserRole::Mahasiswa->value])->group(function () {
+Route::middleware(['auth', 'role:' . UserRole::Mahasiswa->value])->group(function () {
     Route::get('/dashboard-mahasiswa', [DashboardMahasiswaController::class, 'index']);
     Route::resource('/dashboard-mahasiswa/irs', MahasiswaKelolaIrs::class)->only(['index', 'create', 'store', 'update'])->parameters(['irs' => 'irs']);
     Route::resource('/dashboard-mahasiswa/kelola-khs', MahasiswaKelolaKhs::class)->only(['index', 'create', 'store']);
@@ -80,13 +85,15 @@ Route::middleware(['auth', 'role:'.UserRole::Mahasiswa->value])->group(function 
 });
 
 // Admin
-Route::middleware(['auth', 'role:1'])->group(function () {
+Route::middleware(['auth', 'role:' . UserRole::Admin->value])->group(function () {
     Route::get('/dashboard-admin', [DashboardAdminController::class, 'index']);
     Route::get('/dashboard-admin/data-mahasiswa', [DashboardAdminController::class, 'dataMahasiswa']);
-    Route::get('/dashboard-admin/tambah-mahasiswa-baru', [DashboardAdminController::class, 'tambahMahasiswaBaru']);
-    Route::get('/dashboard-admin/tambah-mahasiswa-baru/create', [DashboardAdminController::class, 'createMahasiswa']);
-    Route::resource('/dashboard-admin/semester', AdminKelolaSemester::class);
+    // Route::get('/dashboard-admin/tambah-mahasiswa-baru', [DashboardAdminController::class, 'tambahMahasiswaBaru']);
+    // Route::get('/dashboard-admin/tambah-mahasiswa-baru/create', [DashboardAdminController::class, 'createMahasiswa']);
+    Route::resource('/dashboard-admin/semester', SemesterController::class);
     // Route::get('/dashboard-admin/semester', [DashboardAdminController::class, 'createSemester']);
+    Route::resource('/dashboard-admin/mata-kuliah', MataKuliahController::class);
+    Route::resource('/dashboard-admin/mahasiswa', MahasiswaController::class);
 });
 
 //API Routes
