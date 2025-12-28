916ad2cb4f45a5917807239cf5312fd2569cace0
Fathan <74785057+Churma16@users.noreply.github.com>
2025-12-28 19:37:40 +0700

    Refactor IrsDetail model to define relationships with Irs and MataKuliah models

--- app/Models/IrsDetail.php
+++ app/Models/IrsDetail.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class IrsDetail extends Model
 {
     use HasFactory;
+
+    protected $table = 'irs_details';
+
+    // Detail ini milik satu IRS
+    public function irs()
+    {
+        return $this->belongsTo(Irs::class, 'irs_id');
+    }
+
+    // Detail ini merujuk ke satu Mata Kuliah
+    public function mataKuliah()
+    {
+        return $this->belongsTo(MataKuliah::class, 'mata_kuliah_id');
+    }
 }
